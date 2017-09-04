503a7b9b3d2ca7d6c4bf158469f70b7ef80ad386
Milan Jankovic <MilanJ@kentico.com>
2017-09-04 10:07:54 +0200

    KA-183 Implement tests for GET invalid request, extract config init
    The server/client initialization is required only for a small subset of tests - no reason to go through it in startup with every single unit test.

---
+++ ListApp/ListApp.Api/Utils/ServerInit.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Routing;
+using JsonPatch;
+using JsonPatch.Formatting;
+using JsonPatch.Paths.Resolvers;
+using ListApp.Api.Filters;
+using Microsoft.Web.Http.Routing;
+
+namespace ListApp.Api.Utils
+{
+    public static class ServerInit
+    {
+        public static HttpConfiguration InitializeConfiguration(HttpConfiguration config)
+        {
+            var constraintResolver = new DefaultInlineConstraintResolver()
+            {
+                ConstraintMap =
+                {
+                    ["apiVersion"] = typeof( ApiVersionRouteConstraint )
+                }
+            };
+
+            //   config.BindParameter(typeof(Guid), new GuidModelBinder());
+            config.Filters.Add(new ModelValidationActionFilter());
+            config.Filters.Add(new NullArgumentActionFilter());
+            config.Formatters.Add(new JsonPatchFormatter(new JsonPatchSettings
+            {
+                PathResolver = new CaseInsensitivePropertyPathResolver()
+            }));
+            config.AddApiVersioning();
+            config.MapHttpAttributeRoutes(constraintResolver);
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/v{version:apiVersion}/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            return config;
+        }
+    }
+}

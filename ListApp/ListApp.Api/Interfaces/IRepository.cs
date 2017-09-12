1655d82bded5aad3751f679dd0f43658f1a011d8
Milan Jankovic <MilanJ@kentico.com>
2017-09-12 12:05:36 +0200

    KA-184 Add IRepository interface

---
+++ ListApp/ListApp.Api/Interfaces/IRepository.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Linq;
+using System.Threading.Tasks;
+
+namespace ListApp.Api.Interfaces
+{
+    public interface IRepository<TKeyType, TItemType>
+    {
+        /// <summary>
+        /// A method that returns all keys from repository
+        /// </summary>
+        /// <returns>A collection of all keys</returns>
+        Task<IEnumerable<TKeyType>> GetKeysAsync();
+
+        /// <summary>
+        /// A method that returns matching items. If called without a predicate,
+        /// returns all items in the repository.
+        /// </summary>
+        /// <param name="predicate">A predicate used to select matching items</param>
+        /// <returns>A collection of matching items</returns>
+        Task<IEnumerable<TItemType>> GetAllAsync(Func<TItemType, bool> predicate = null);
+
+        /// <summary>
+        /// A method that returns a specific item identified by given key.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns>The item with given key, null if no such item exists.</returns>
+        Task<TItemType> GetAsync(TKeyType key);
+
+        /// <summary>
+        /// Stores given entity in the repository under given key.
+        /// </summary>
+        /// <param name="key">The key used to identify the entity</param>
+        /// <param name="entity">The entity to store in the repository</param>
+        /// <exception cref="ArgumentNullException">if a null argument is passed</exception>
+        /// <exception cref="DuplicateKeyException">if an item with given key already exists</exception>
+        Task AddAsync(TKeyType key, TItemType entity);
+
+        /// <summary>
+        /// Deletes entity with corresponding key. Does nothing when given key does
+        /// not correspond to any item - the goal has been met, no item with given
+        /// key remains in the repository.
+        /// </summary>
+        /// <param name="key">The key used to identify the entity</param>
+        /// <exception cref="ArgumentNullException">if a null argument is passed</exception>
+        Task DeleteAsync(TKeyType key);
+
+        /// <summary>
+        /// Removes all entities from the repository
+        /// </summary>
+        Task ClearAsync();
+    }
+}

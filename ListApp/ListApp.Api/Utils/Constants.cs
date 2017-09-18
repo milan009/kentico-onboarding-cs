96539d00e51b2e7e81a129765006458ab2f29724
Milan Jankovic <MilanJ@kentico.com>
2017-09-18 18:00:26 +0200

    KA-183 Refactor the constants - items are now private

--- ListApp/ListApp.Api/Utils/Constants.cs
+++ ListApp/ListApp.Api/Utils/Constants.cs
@@ -5,17 +5,13 @@ namespace ListApp.Api.Utils
 {
     public static class Constants
     {
-        private static readonly Guid Guid1 = Guid.Parse("00000000-0000-0000-0000-000000000000");
-        private static readonly Guid Guid2 = Guid.Parse("00000000-0000-0000-0000-000000000001");
-        private static readonly Guid Guid3 = Guid.Parse("00000000-0000-0000-0000-000000000002");
+        private static readonly ListItem Item1 = new ListItem { Id = Guid.Parse("00000000-0000-0000-0000-000000000000"), Text = "Stretch correctly" };
+        private static readonly ListItem Item2 = new ListItem { Id = Guid.Parse("00000000-0000-0000-0000-000000000001"), Text = "Make a coffey" };
+        private static readonly ListItem Item3 = new ListItem { Id = Guid.Parse("00000000-0000-0000-0000-000000000002"), Text = "Take over the world" };
+
         public static readonly Guid NonExistingItemGuid = Guid.Parse("00000000-0000-0000-0000-000000000003");
 
-        public static readonly ListItem[] MockListItems =
-        {
-            new ListItem {Id = Guid1, Text = "Stretch correctly"},
-            new ListItem {Id = Guid2, Text = "Make a coffey"},
-            new ListItem {Id = Guid3, Text = "Take over the world"}
-        };
+        public static readonly ListItem[] MockListItems = { Item1, Item2, Item3 };
 
         public static readonly ListItem CreatedListItem = new ListItem
         {

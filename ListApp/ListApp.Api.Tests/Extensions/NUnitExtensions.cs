30d241fe8f762d0402e2264e42f07a5342938037
Milan Jankovic <MilanJ@kentico.com>
2017-09-19 18:28:48 +0200

    KA-183 Include the comparer as a private class of the extension

--- ListApp/ListApp.Api.Tests/Extensions/NUnitExtensions.cs
+++ ListApp/ListApp.Api.Tests/Extensions/NUnitExtensions.cs
@@ -1,4 +1,5 @@
-﻿using ListApp.Api.Tests.Comparers;
+﻿using System.Collections.Generic;
+using ListApp.Api.Models;
 using NUnit.Framework.Constraints;
 
 namespace ListApp.Api.Tests.Extensions
@@ -8,6 +9,26 @@ namespace ListApp.Api.Tests.Extensions
         public static EqualConstraint UsingListItemComparer(this EqualConstraint constr)
         {
             return constr.Using(ListItemEqualityComparer.Instance);
+        }
+
+        private class ListItemEqualityComparer : IEqualityComparer<ListItem>
+        {
+            private ListItemEqualityComparer() { }
+
+            public static ListItemEqualityComparer Instance { get; }
+                = new ListItemEqualityComparer();
+
+            public bool Equals(ListItem x, ListItem y)
+            {
+                if (ReferenceEquals(x, y)) return true;
+                if (x == null || y == null) return false;
+                return x.Id == y.Id && x.Text == y.Text;
+            }
+
+            public int GetHashCode(ListItem obj)
+            {
+                return obj.GetHashCode();
+            }
         }
     }
 }

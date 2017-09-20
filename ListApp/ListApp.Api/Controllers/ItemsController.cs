c18a299f4009113e1e348a8291f4c2de8ff5da0d
Milan Jankovic <MilanJ@kentico.com>
2017-09-20 09:02:55 +0200

    KA-183 Add spaces to attributes vol. II

--- ListApp/ListApp.Api/Controllers/ItemsController.cs
+++ ListApp/ListApp.Api/Controllers/ItemsController.cs
@@ -20,19 +20,19 @@ namespace ListApp.Api.Controllers
                 => await Task.FromResult(Ok(Constants.MockListItems));
 
             [Route("{id}")]
-            public async Task<IHttpActionResult> GetAsync([FromUri] Guid id) 
+            public async Task<IHttpActionResult> GetAsync([FromUri] Guid id)
                 => await Task.FromResult(Ok(Constants.MockListItems.ElementAt(0)));
 
             [Route]
-            public async Task<IHttpActionResult> PostAsync([FromBody] ListItem newItem) 
+            public async Task<IHttpActionResult> PostAsync([FromBody] ListItem newItem)
                 => await Task.FromResult(Created(Url.Request.RequestUri + $"/{Constants.NonExistingItemGuid}", Constants.CreatedListItem));
 
             [Route("{id}")]
-            public async Task<IHttpActionResult> PutAsync([FromUri] Guid id, [FromBody]ListItem newItem) 
+            public async Task<IHttpActionResult> PutAsync([FromUri] Guid id, [FromBody]ListItem newItem)
                 => await Task.FromResult(Created(Url.Request.RequestUri + $"/{Constants.NonExistingItemGuid}", Constants.CreatedListItem));
 
             [Route("{id}")]
-            public async Task<IHttpActionResult> DeleteAsync([FromUri] Guid id) 
+            public async Task<IHttpActionResult> DeleteAsync([FromUri] Guid id)
                 => await Task.FromResult(StatusCode(HttpStatusCode.NoContent));
         }
     }

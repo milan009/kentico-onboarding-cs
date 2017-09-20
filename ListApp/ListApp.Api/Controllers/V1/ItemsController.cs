3e6df7aa2c28067c9d35a93fc4504d2925be4d53
Milan Jankovic <MilanJ@kentico.com>
2017-09-20 09:30:13 +0200

    KA-183 Fix namespace in ItemsController

--- ListApp/ListApp.Api/Controllers/V1/ItemsController.cs
+++ ListApp/ListApp.Api/Controllers/V1/ItemsController.cs
@@ -7,33 +7,30 @@ using ListApp.Api.Models;
 using ListApp.Api.Utils;
 using Microsoft.Web.Http;
 
-namespace ListApp.Api.Controllers
+namespace ListApp.Api.Controllers.V1
 {
-    namespace V1
+    [ApiVersion("1.0")]
+    [RoutePrefix("api/v{version:apiVersion}/items")]
+    public class ItemsController : ApiController
     {
-        [ApiVersion("1.0")]
-        [RoutePrefix("api/v{version:apiVersion}/items")]
-        public class ItemsController : ApiController
-        {
-            [Route]
-            public async Task<IHttpActionResult> GetAsync()
-                => await Task.FromResult(Ok(Constants.MockListItems));
+        [Route]
+        public async Task<IHttpActionResult> GetAsync()
+            => await Task.FromResult(Ok(Constants.MockListItems));
 
-            [Route("{id}")]
-            public async Task<IHttpActionResult> GetAsync([FromUri] Guid id)
-                => await Task.FromResult(Ok(Constants.MockListItems.ElementAt(0)));
+        [Route("{id}")]
+        public async Task<IHttpActionResult> GetAsync([FromUri] Guid id)
+            => await Task.FromResult(Ok(Constants.MockListItems.ElementAt(0)));
 
-            [Route]
-            public async Task<IHttpActionResult> PostAsync([FromBody] ListItem newItem)
-                => await Task.FromResult(Created(Url.Request.RequestUri + $"/{Constants.NonExistingItemGuid}", Constants.CreatedListItem));
+        [Route]
+        public async Task<IHttpActionResult> PostAsync([FromBody] ListItem newItem)
+            => await Task.FromResult(Created(Url.Request.RequestUri + $"/{Constants.NonExistingItemGuid}", Constants.CreatedListItem));
 
-            [Route("{id}")]
-            public async Task<IHttpActionResult> PutAsync([FromUri] Guid id, [FromBody]ListItem newItem)
-                => await Task.FromResult(Created(Url.Request.RequestUri + $"/{Constants.NonExistingItemGuid}", Constants.CreatedListItem));
+        [Route("{id}")]
+        public async Task<IHttpActionResult> PutAsync([FromUri] Guid id, [FromBody]ListItem newItem)
+            => await Task.FromResult(Created(Url.Request.RequestUri + $"/{Constants.NonExistingItemGuid}", Constants.CreatedListItem));
 
-            [Route("{id}")]
-            public async Task<IHttpActionResult> DeleteAsync([FromUri] Guid id)
-                => await Task.FromResult(StatusCode(HttpStatusCode.NoContent));
-        }
+        [Route("{id}")]
+        public async Task<IHttpActionResult> DeleteAsync([FromUri] Guid id)
+            => await Task.FromResult(StatusCode(HttpStatusCode.NoContent));
     }
 }

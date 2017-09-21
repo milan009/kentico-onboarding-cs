be646dba91eb4df2c08c06440a1a0559061d3c9f
Milan Jankovic <MilanJ@kentico.com>
2017-09-21 15:05:32 +0200

    KA-184 Fix namespace in ItemsController

--- ListApp/ListApp.Api/Controllers/V1/ItemsController.cs
+++ ListApp/ListApp.Api/Controllers/V1/ItemsController.cs
@@ -6,62 +6,59 @@ using ListApp.Api.Interfaces;
 using ListApp.Api.Models;
 using Microsoft.Web.Http;
 
-namespace ListApp.Api.Controllers
+namespace ListApp.Api.Controllers.V1
 {
-    namespace V1
+    [ApiVersion("1.0")]
+    [RoutePrefix("api/v{version:apiVersion}/items")]
+    public class ItemsController : ApiController
     {
-        [ApiVersion("1.0")]
-        [RoutePrefix("api/v{version:apiVersion}/items")]
-        public class ItemsController : ApiController
+        #region HTTP verbs implementations
+
+        private readonly IRepository<Guid, ListItem> _repository;
+        private readonly IGuidGenerator _guidGenerator;
+
+
+        public ItemsController(IRepository<Guid, ListItem> repository, IGuidGenerator guidGenerator)
         {
-            #region HTTP verbs implementations
-
-            private readonly IRepository<Guid, ListItem> _repository;
-            private readonly IGuidGenerator _guidGenerator;
-
-
-            public ItemsController(IRepository<Guid, ListItem> repository, IGuidGenerator guidGenerator)
-            {
-                _repository = repository;
-                _guidGenerator = guidGenerator;
-            }
-
-            [Route]
-            public async Task<IHttpActionResult> GetAsync()
-                => Ok(await _repository.GetAllAsync());
-
-            [Route("{id}")]
-            public async Task<IHttpActionResult> GetAsync([FromUri]Guid id) 
-                => Ok(await _repository.GetAsync(id));
-
-            [Route]
-            public async Task<IHttpActionResult> PostAsync([FromBody] ListItem newItem)
-            {
-                // Generate the guid - dummy functionality, no need to do checks.
-                newItem.Id = _guidGenerator.GenerateGuid();
-                await _repository.AddAsync(newItem.Id, newItem);
-
-                return Created($"/items/{newItem.Id}", newItem);
-            }
-
-            [Route("{id}")]
-            public async Task<IHttpActionResult> PutAsync([FromUri] Guid id, [FromBody] ListItem newItem)
-            {
-                await _repository.DeleteAsync(id);
-                await _repository.AddAsync(id, newItem);
-
-                return Created($"/items/{id}", newItem);
-            }
-
-            [Route("{id}")]
-            public async Task<IHttpActionResult> DeleteAsync([FromUri] Guid id)
-            {
-                await _repository.DeleteAsync(id);
-
-                return StatusCode(HttpStatusCode.NoContent);
-            }
-
-            #endregion
+            _repository = repository;
+            _guidGenerator = guidGenerator;
         }
-    }
+
+        [Route]
+        public async Task<IHttpActionResult> GetAsync()
+            => Ok(await _repository.GetAllAsync());
+
+        [Route("{id}")]
+        public async Task<IHttpActionResult> GetAsync([FromUri] Guid id) 
+            => Ok(await _repository.GetAsync(id));
+
+        [Route]
+        public async Task<IHttpActionResult> PostAsync([FromBody] ListItem newItem)
+        {
+            // Generate the guid - dummy functionality, no need to do checks.
+            newItem.Id = _guidGenerator.GenerateGuid();
+            await _repository.AddAsync(newItem.Id, newItem);
+
+            return Created($"/items/{newItem.Id}", newItem);
+        }
+
+        [Route("{id}")]
+        public async Task<IHttpActionResult> PutAsync([FromUri] Guid id, [FromBody] ListItem newItem)
+        {
+            await _repository.DeleteAsync(id);
+            await _repository.AddAsync(id, newItem);
+
+            return Created($"/items/{id}", newItem);
+        }
+
+        [Route("{id}")]
+        public async Task<IHttpActionResult> DeleteAsync([FromUri] Guid id)
+        {
+            await _repository.DeleteAsync(id);
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        #endregion
+    } 
 }

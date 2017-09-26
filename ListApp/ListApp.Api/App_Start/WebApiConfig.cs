f006f9446aa5903eb0cd369053557704cca88b39
Milan Jankovic <MilanJ@kentico.com>
2017-09-26 16:16:44 +0200

    KA-184 Move the unity resolver back to Api project

--- ListApp/ListApp.Api/App_Start/WebApiConfig.cs
+++ ListApp/ListApp.Api/App_Start/WebApiConfig.cs
@@ -5,6 +5,7 @@ using ListApp.Contracts.Interfaces;
 using ListApp.Contracts.Models;
 using ListApp.Repositories;
 using ListApp.Api.Services;
+using ListApp.Api.Utils;
 using ListApp.Utils;
 using Microsoft.Practices.Unity;
 using Microsoft.Web.Http.Routing;
--- ListApp/ListApp.Api/Utils/UnityResolver.cs
+++ ListApp/ListApp.Api/Utils/UnityResolver.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Web.Http.Dependencies;
+using Microsoft.Practices.Unity;
 
 namespace ListApp.Api.Utils
 {

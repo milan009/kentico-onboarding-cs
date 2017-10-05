9649401d57dee600c7ea7ab6636a02c83ef06bcb
Milan Jankovic <MilanJ@kentico.com>
2017-10-05 08:59:54 +0200

    KA-184 Remove the Utils project

--- ListApp/Tests/ListApp.Api.Tests/ItemControllerTests.cs
+++ ListApp/Tests/ListApp.Api.Tests/ItemControllerTests.cs
@@ -1,17 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Web.Http;
 using ListApp.Api.Controllers.V1;
-using ListApp.Api.Services.RouteHelper;
 using ListApp.Contracts.Interfaces;
 using ListApp.Contracts.Models;
 using ListApp.Api.Tests.Extensions;
-using ListApp.Utils;
 using NSubstitute;
 using NUnit.Framework;
 
@@ -32,20 +29,7 @@ namespace ListApp.Api.Tests
         public void SetUp()
         {
             _itemsRepository = Substitute.For<IRepository>();
-            _itemsRepository.GetAllAsync()
-                .Returns(Constants.MockListItems);
-            _itemsRepository.GetAsync(Arg.Any<Guid>())
-                .Returns(Constants.MockListItems.ElementAt(0));
-            _itemsRepository.AddAsync(Arg.Any<ListItem>())
-                .Returns(Constants.MockListItems.ElementAt(0));
-            _itemsRepository.DeleteAsync(Arg.Any<Guid>())
-                .Returns(Constants.MockListItems.ElementAt(0));
-            _itemsRepository.UpdateAsync(Arg.Any<ListItem>())
-                .Returns(Constants.MockListItems.ElementAt(0));
-
             _routeHelper = Substitute.For<IRouteHelper>();
-            _routeHelper.GetItemUrl(Arg.Any<Guid>())
-                .Returns(Guid.Empty.ToString());
 
             _itemsController =
                 new ItemsController(_itemsRepository, _routeHelper)
@@ -67,10 +51,11 @@ namespace ListApp.Api.Tests
             const HttpStatusCode expectedResponseCode = HttpStatusCode.OK;
             var expectedItems = new []
             {
-                new ListItem {Id = Guid.Empty, Text = "Stretch correctly"},
-                new ListItem {Id = Guid.Parse("00000000-0000-0000-0000-000000000001"), Text = "Make a coffey"},
-                new ListItem {Id = Guid.Parse("00000000-0000-0000-0000-000000000002"), Text = "Take over the world"}
+                new ListItem {Id = Guid.Parse("BFBF8A62-FD82-42D4-A86B-324704BE161E"), Text = "Stretch correctly"},
+                new ListItem {Id = Guid.Parse("A55578BC-57F2-4A42-BEDF-7D8C23992DBC"), Text = "Make coffee"},
+                new ListItem {Id = Guid.Parse("C6F4D46F-D7B1-45DD-8C7C-265313AF77BB"), Text = "Take over the world"}
             };
+            _itemsRepository.GetAllAsync().ReturnsForAnyArgs(expectedItems);
 
             var receivedResponse = await _itemsController.GetAsync();
             var responseMessage = await receivedResponse.ExecuteAsync(CancellationToken.None);
@@ -87,9 +72,10 @@ namespace ListApp.Api.Tests
             const HttpStatusCode expectedResponseCode = HttpStatusCode.OK;
             var expectedItem = new ListItem
             {
-                Id = Guid.Empty,
+                Id = Guid.Parse("BFBF8A62-FD82-42D4-A86B-324704BE161E"),
                 Text = "Stretch correctly"
             };
+            _itemsRepository.GetAsync(Arg.Any<Guid>()).Returns(expectedItem);
 
             var receivedResponse = await _itemsController.GetAsync(Guid.NewGuid());
             var responseMessage = await receivedResponse.ExecuteAsync(CancellationToken.None);
@@ -107,9 +93,11 @@ namespace ListApp.Api.Tests
             const HttpStatusCode expectedResponseCode = HttpStatusCode.Created;
             var expectedItem = new ListItem
             {
-                Id = Guid.Empty,
+                Id = Guid.Parse("BFBF8A62-FD82-42D4-A86B-324704BE161E"),
                 Text = "Stretch correctly"
             };
+            _itemsRepository.AddAsync(Arg.Any<ListItem>()).Returns(expectedItem);
+            _routeHelper.GetItemUrl(Arg.Any<Guid>()).Returns(expectedLocation);
 
             var receivedResponse = await _itemsController.PostAsync(PostedItem);
             var responseMessage = await receivedResponse.ExecuteAsync(CancellationToken.None);
@@ -127,9 +115,10 @@ namespace ListApp.Api.Tests
             const HttpStatusCode expectedResponseCode = HttpStatusCode.OK;
             var expectedItem = new ListItem
             {
-                Id = Guid.Empty,
+                Id = Guid.Parse("BFBF8A62-FD82-42D4-A86B-324704BE161E"),
                 Text = "Stretch correctly"
             };
+            _itemsRepository.UpdateAsync(Arg.Any<ListItem>()).Returns(expectedItem);
 
             var receivedResponse = await _itemsController.PutAsync(PostedItemGuid, PostedItem);
             var responseMessage = await receivedResponse.ExecuteAsync(CancellationToken.None);
@@ -149,6 +138,7 @@ namespace ListApp.Api.Tests
                 Id = Guid.Empty,
                 Text = "Stretch correctly"
             };
+            _itemsRepository.DeleteAsync(Arg.Any<Guid>()).Returns(expectedItem);
 
             var receivedResponse = await _itemsController.DeleteAsync(PostedItemGuid);
             var responseMessage = await receivedResponse.ExecuteAsync(CancellationToken.None);

860237b602d42ab781ee82f30ac09a517420f183
Milan Jankovic <MilanJ@kentico.com>
2017-10-09 16:35:47 +0200

    KA-185 Extend the model with timestamps

--- ListApp/Sources/ListApp.Contracts/Models/ListItem.cs
+++ ListApp/Sources/ListApp.Contracts/Models/ListItem.cs
@@ -6,5 +6,7 @@ namespace ListApp.Contracts.Models
     {
         public Guid Id { get; set; }
         public string Text { get; set; }
+        public DateTime? Created { get; set; }
+        public DateTime? LastModified { get; set; }
     }
 }

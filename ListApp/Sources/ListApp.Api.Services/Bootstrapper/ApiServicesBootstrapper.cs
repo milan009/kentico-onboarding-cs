d2b739704f7235e24937424bf325db7a3d2dccd2
Milan Jankovic <MilanJ@kentico.com>
2017-10-09 14:17:03 +0200

    KA-185 Merge service bootstrappers
    This commit is made to simplify merging with task-2 branch

---
+++ ListApp/Sources/ListApp.Api.Services/Bootstrapper/ApiServicesBootstrapper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ListApp.Api.Services.GuidGenerator;
+using ListApp.Api.Services.RouteHelper;
+using ListApp.Contracts.Interfaces;
+using Microsoft.Practices.Unity;
+
+namespace ListApp.Api.Services.Bootstrapper
+{
+    class ApiServicesBootstrapper : IUnityContainerBootstrapper
+    {
+        public IUnityContainer RegisterTypes(IUnityContainer container)
+        {
+            return container
+                .RegisterType<IGuidGenerator, GuidGenerator.GuidGenerator>(new HierarchicalLifetimeManager())
+                .RegisterType<IRouteHelper, RouteHelper.RouteHelper>(new HierarchicalLifetimeManager());
+        }
+    }
+}

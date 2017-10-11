16f3a49ef2c09f693785ca4c9841ea4bb347d19f
Milan Jankovic <MilanJ@kentico.com>
2017-10-11 12:49:34 +0200

    KA-185 Add static "failed" result

--- ListApp/Sources/ListApp.Contracts/Models/OperationResult.cs
+++ ListApp/Sources/ListApp.Contracts/Models/OperationResult.cs
@@ -1,11 +1,25 @@
-﻿namespace ListApp.Contracts.Models
+﻿using System;
+
+namespace ListApp.Contracts.Models
 {
     public class OperationResult
     {
+        public static readonly OperationResult Failed = new OperationResult(false, null);
+
+        public static OperationResult CreateSuccessfulResult(ListItem item)
+        {
+            if (item == null)
+            {
+                throw new ArgumentNullException(nameof(item));
+            }
+
+            return new OperationResult(true, item);
+        }
+
         public bool Found { get; }
         public ListItem Item { get; }
 
-        public OperationResult(bool found, ListItem item)
+        private OperationResult(bool found, ListItem item)
         {
             Found = found;
             Item = item;
